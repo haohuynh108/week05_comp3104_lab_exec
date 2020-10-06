4bb44beae1bca3ce5845e08cd58a195d0738a825
haohuynh108 <giahao10893@gmail.com>
2020-10-05 20:44:13 -0400

    DevOps - week 05 lab - scripts

---
+++ build/precache-manifest.e83185b1fdcc606fd98e523ee958d8c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "419ad8c0232336557ca1",
+    "url": "/static/css/main.168ceaa3.chunk.css"
+  },
+  {
+    "revision": "419ad8c0232336557ca1",
+    "url": "/static/js/main.419ad8c0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "ff2bdbaed0b9afcc857ae029709b3355",
+    "url": "/index.html"
+  }
+];
